662e05057c7b7542a0c1e4505e0a8e30e00ed320
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-05-16 00:03:44 +0600

    module file system

---
+++ src/module/user/user.controller.ts
@@ -0,0 +1,120 @@
+import type { Request, Response } from "express"
+import { pool } from "../../db"
+import { userService } from "./user.service"
+
+const createUser = async (req: Request, res: Response) => {
+    // console.log(req.body);
+    // const { name, age, email, password } = req.body
+    try {
+        const result = await userService.createUserIntoDB(req.body)
+        res.status(201).json({
+            message: "User Created Successfully",
+            data: result.rows[0]
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            message: error.message,
+            error: error
+        })
+    }
+
+}
+const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const result = await userService.getAllUsersFromDB()
+        res.status(200).json({
+            success: true,
+            message: "User retrived successfully",
+            data: result.rows
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            data: error
+        })
+    }
+}
+const getSingleUser = async (req: Request, res: Response) => {
+    const { id } = req.params
+    console.log(id);
+    try {
+        const result = await userService.getSingleUserFromDB(id as string)
+        if(result.rows.length === 0){
+            res.status(404).json({
+            success: false,
+            message: "User Not Found",
+            data: {}
+        })
+        }
+        res.status(200).json({
+            success: true,
+            message: "User retrived successfully",
+            data: result.rows[0]
+        })
+    } catch (error :any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            data: error
+        })
+    }
+
+}
+
+const updateUser = async (req: Request, res: Response) => {
+    const {id} = req.params
+    // const {name,age,is_active,password} = req.body
+    // console.log(id, name, age, email,is_active);
+    try {
+        const result = await userService.updateUserFromDB(req.body,id as string)
+        if (result.rows.length === 0) {
+             res.status(404).json({
+            success: false,
+            message: "User Not Found",
+        })
+        }
+        res.status(200).json({
+            success: true,
+            message: "User Updated successfully",
+            data: result.rows[0]
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            data: error
+        })
+    }
+    
+}
+const deleteUser = async (req:Request, res: Response) => {
+    const {id} = req.params
+
+    const result = await userService.deleteUserFromDB(id as string)
+    if (result.rowCount === 0) {
+             res.status(404).json({
+            success: false,
+            message: "User Not Found",
+        })
+        }
+    try {
+        res.status(200).json({
+            success: true,
+            message: "User Deleted successfully",
+        })
+    } catch (error : any) {
+        res.status(500).json({
+            status: false,
+            message: error.message,
+            error: error
+        })
+    }
+}
+export const userController = {
+    createUser,
+    getAllUsers,
+    getSingleUser,
+    updateUser,
+    deleteUser
+}
--- src/module/user/user.router.ts
+++ src/module/user/user.router.ts
@@ -1,29 +1,12 @@
-import { Router, type Request, type Response } from "express";
-import { pool } from "../../db";
+import { Router } from "express";
+import { userController } from "./user.controller";
 
 const router = Router()
 
-router.post("/api/users", async (req: Request, res: Response) => {
-    // console.log(req.body);
-    const { name, age, email, password } = req.body
-    try {
-        const result = await pool.query(
-            `
-        INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4)
-        RETURNING *
-        `, [name, email, password, age],
-        )
-        res.status(201).json({
-            message: "User Created Successfully",
-            data: result.rows[0]
-        })
-    } catch (error: any) {
-        res.status(500).json({
-            message: error.message,
-            error: error
-        })
-    }
-
-})
+router.post("/", userController.createUser)
+router.get("/", userController.getAllUsers)
+router.get("/:id", userController.getSingleUser)
+router.put("/:id",userController.updateUser)
+router.delete("/:id",userController.deleteUser)
 
 export const userRoute = router
